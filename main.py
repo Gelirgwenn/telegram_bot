d84b0e6c7c09ce7e0c465f56aa23f9388e3a1a89
shvarts003@gmail.com <Shvartsqaz12>
2024-01-30 17:15:53 +0300

    добавил функцию минус

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+def mult(a, b):
+    return a * b
 
-def mult(a,b):
-    return a*b
+
+def min(a, b):
+    return a - b
 
 
 if __name__ == '__main__':
-    print(mult(2,5))
+    print(mult(2, 5))
+    print(min(9, 3))
