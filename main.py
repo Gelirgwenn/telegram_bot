604d47182169937eff63f41244d8a3c112109e60
shvarts003@gmail.com <Shvartsqaz12>
2024-01-30 16:35:47 +0300

    добавил первую функцию

--- main.py
+++ main.py
@@ -4,13 +4,21 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+#def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('test')
+  #  print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#    print('test')
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+def mult(a,b):
+    return a*b
+
+
+if __name__ == '__main__':
+    print(mult(2,5))
